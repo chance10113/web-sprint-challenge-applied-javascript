da5bfef7d713c3f251674362d130dae508c35d75
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-01-15 10:58:10 -0600

    Finished Tabs

--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,35 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+
+const tabEntryPoint = document.querySelector('.topics')
+
+axios
+    .get('https://lambda-times-api.herokuapp.com/topics')
+    .then((res) => {
+        // console.log('res', res)
+        // console.log('res.data', res.data)
+        // console.log('res.data.topics', res.data.topics)
+
+        let tabData = res.data.topics
+// Tab Making Function
+        function tabMaker(tabData) {
+        // Create Elements
+            const tab = document.createElement('div')
+        // Adding Classes
+            tab.classList.add('tab')
+        // Assigning Content
+            tab.innerText = tabData
+            tabEntryPoint.appendChild(tab)
+        }
+// Mapping Through Data
+        const tabMap = tabData.map(tabData => {
+            return tabMaker(tabData)
+        })
+        
+    })
+    .catch((error) => {
+        console.log('Tab Axios Broke', error)
+    })
+    //Gonna need to create a var for res.data, 
+    //then feed it through a forEach(?), then append all to entry
